c4e149c3d7f4404249a33acbb2b871511059db42
luis dourado <lfaddourado@gmail.com>
2023-05-29 10:28:25 -0300

    feat: trying to get isr working on aws

--- src/actions/usersQuantity.ts
+++ src/actions/usersQuantity.ts
@@ -1,8 +1,13 @@
 'use server';
 
+import { revalidate } from '@/app/api/quantity/route';
+
 export default async function UsersQuantity(selectedQuantity: number) {
   const getRandomUsers = await fetch(
-    'https://randomuser.me/api/?results=' + selectedQuantity
+    'https://randomuser.me/api/?results=' + selectedQuantity,
+    {
+      next: { revalidate: 10 },
+    }
   );
   const randomUsers = await getRandomUsers.json();
   return randomUsers;
--- src/components/QuantityInput.tsx
+++ src/components/QuantityInput.tsx
@@ -12,7 +12,7 @@ interface QuantityInputProps {
   data: number[];
 }
 export default function QuantityInput(data: QuantityInputProps) {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<any>([]);
   const [selectedUser, setSelectedUser] = useState(null);
   return (
     <>
@@ -53,8 +53,7 @@ export default function QuantityInput(data: QuantityInputProps) {
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           onChange={(e) => {
             const user = users.find(
-              // @ts-expect-error too lazy to type this
-              (item) => item.name.first === e.target.value
+              (item: any) => item.name.first === e.target.value
             );
             if (!user) return;
             setSelectedUser(user);
@@ -62,48 +61,43 @@ export default function QuantityInput(data: QuantityInputProps) {
         >
           <option defaultValue={'Choose your user'}>Choose your user</option>
           {users.length > 0 ? (
-            users.map((item) => (
-              // @ts-expect-error too lazy to type this
-              <option key={item.login.uuid}>
-                {/* @ts-expect-error too lazy to type this */}
-                {item.name.first}
-              </option>
+            users.map((item: any) => (
+              <option key={item.login.uuid}>{item.name.first}</option>
             ))
           ) : (
             <option>Choose your user first</option>
           )}
         </select>
-      </div>
+        {selectedUser && (
+          <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <Image
+              className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+              // @ts-ignore
+              src={selectedUser.picture.large}
+              alt="user"
+              width={240}
+              height={240}
+            />
+            <div className="flex flex-col justify-between p-4 leading-normal">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {/* @ts-ignore */}
+                {selectedUser.name.first} {selectedUser.name.last}
+              </h5>
 
-      {selectedUser && (
-        <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <Image
-            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-            // @ts-ignore
-            src={selectedUser.picture.large}
-            alt="user"
-            width={240}
-            height={240}
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {/* @ts-ignore */}
-              {selectedUser.name.first} {selectedUser.name.last}
-            </h5>
-
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              {/* @ts-ignore */}
-              Email: {selectedUser.email} <br />
-              {/* @ts-ignore */}
-              Naturality: {selectedUser.nat} <br />
-              {/* @ts-ignore */}
-              Country: {selectedUser.location.country} <br />
-              {/* @ts-ignore */}
-              State: {selectedUser.location.state} <br />
-            </p>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {/* @ts-ignore */}
+                Email: {selectedUser.email} <br />
+                {/* @ts-ignore */}
+                Naturality: {selectedUser.nat} <br />
+                {/* @ts-ignore */}
+                Country: {selectedUser.location.country} <br />
+                {/* @ts-ignore */}
+                State: {selectedUser.location.state} <br />
+              </p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 }
